deef99812c2b1855bf5a5056650f63f279fc122c
buttonbugs <miller.chen2006@outlook.com>
2026-03-20 18:53:01 -0700

    Fix low speed upside-down leftright

--- src/motor_low_speed.cpp
+++ src/motor_low_speed.cpp
@@ -1,11 +1,16 @@
 #include "melty_config.h"
 #include "rc_handler.h"
 #include "motor_driver.h"
+#include "accel_handler.h"
 
 void low_speed_set_motor() {
     // Return ratio from -1.0 to 1.0
     float forback_ratio = rc_get_forback_ratio();
     float leftright_ratio = rc_get_leftright_ratio();
+
+    if (!is_facing_up) {
+        leftright_ratio = -leftright_ratio;
+    }
 
     // The following parameters might be negative. Please turn on ENABLE_REVERSE in melty_config.h before calling low_speed_set_motor()
     motor_1_on(forback_ratio * LOW_SPEED_FORBACK_THROTTLE_PERCENT + leftright_ratio * LOW_SPEED_REVOLUTION_THROTTLE_PERCENT);
