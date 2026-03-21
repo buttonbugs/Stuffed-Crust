bbbacdeefa8e003b0128d6079f26ffbf748208bd
buttonbugs <miller.chen2006@outlook.com>
2026-03-20 21:15:19 -0700

    Adjust gyro z offset

--- src/melty_config.h
+++ src/melty_config.h
@@ -21,7 +21,7 @@
 #define DEFAULT_ACCEL_MOUNT_RADIUS_CM 2.28        //Radius of accelerometer from center of robot
 #define DEFAULT_LED_OFFSET_PERCENT 13              //Adjust to make heading LED line up with direction robot travels 0-99 (increasing moves beacon clockwise)
                       
-#define DEFAULT_ACCEL_ZERO_G_OFFSET_Z 1.4f
+#define DEFAULT_ACCEL_ZERO_G_OFFSET_Z 2.7f
 #define DEFAULT_ACCEL_ZERO_G_OFFSET_X 0.0f          //Value accelerometer returns with robot at rest (in G) - adjusts for any offset
                                                   //H3LIS331 claims +/-1g DC offset - typical - but +/-2.5 has been observed at +/-400g setting (enough to cause tracking error)
                                                   //Just enterring and exiting config mode will automatically set this value / save to EEPROM (based on current accel reading reflecting 0g)
