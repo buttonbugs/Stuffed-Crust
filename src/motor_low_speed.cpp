491c49731e0238e6ce270e8777e0edd8ee1dd71b
buttonbugs <miller.chen2006@outlook.com>
2026-03-20 18:55:14 -0700

    Enhance precise control

--- src/motor_low_speed.cpp
+++ src/motor_low_speed.cpp
@@ -1,3 +1,5 @@
+#include <Arduino.h>
+
 #include "melty_config.h"
 #include "rc_handler.h"
 #include "motor_driver.h"
@@ -5,8 +7,8 @@
 
 void low_speed_set_motor() {
     // Return ratio from -1.0 to 1.0
-    float forback_ratio = rc_get_forback_ratio();
-    float leftright_ratio = rc_get_leftright_ratio();
+    float forback_ratio = pow(rc_get_forback_ratio(), 3.0f);
+    float leftright_ratio = pow(rc_get_leftright_ratio(), 3.0f);
 
     if (!is_facing_up) {
         leftright_ratio = -leftright_ratio;
