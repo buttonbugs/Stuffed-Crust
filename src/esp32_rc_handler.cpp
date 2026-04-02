5c9f32ae3bd81fbd770b7480e4205c2bddadf57a
buttonbugs <miller.chen2006@outlook.com>
2026-04-01 19:06:59 -0700

    Add a esp32 rc channel for robot facing

--- src/esp32_rc_handler.cpp
+++ src/esp32_rc_handler.cpp
@@ -1,9 +1,10 @@
 #include "melty_config.h"
 
-float throttle_ratio = 0.0;   
-float forback_ratio = 0.0;    
-float leftright_ratio = 0.0;  
-float revolution_ratio = 0.0; 
+float throttle_ratio = 0.0;
+float forback_ratio = 0.0;
+float leftright_ratio = 0.0;
+float revolution_ratio = 0.0;
+bool facing_up = true;
 
 // https://github.com/ricardoquesada/bluepad32?tab=readme-ov-file
 // return float from 0.0 to 1.0 based on stick position
@@ -24,4 +25,8 @@ void rc_get_leftright_ratio_esp32() {
 // return float from -1.0 to 1.0 based on stick position
 void rc_get_revolution_ratio_esp32() {
     // Set the variable revolution_ratio
+}
+
+void rc_get_facing_esp32() {
+    // Set the variable facing_up
 }
--- src/esp32_rc_handler.h
+++ src/esp32_rc_handler.h
@@ -5,5 +5,6 @@ extern float throttle_ratio;            // return float from 0.0 to 1.0 based on
 extern float forback_ratio;             // return float from -1.0 to 1.0 based on stick position``
 extern float leftright_ratio;           // return float from -1.0 to 1.0 based on stick position``
 extern float revolution_ratio;          // return float from -1.0 to 1.0 based on stick position``
+extern bool facing_up;                  // return true/false based on the switch on the xbox controller
 
 #endif
